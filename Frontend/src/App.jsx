4d3eae0b331aa0cde9317317a1c8c13ce226bca6
Bookmytowing <bookmytowing@gmail.com>
2024-07-23 12:17:53 +0530

    kishan branch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       street2site
       change 2
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur laudantium similique nesciunt unde rerum ea vel, deleniti, corrupti quas atque fugiat animi nobis qui repellendus cupiditate deserunt culpa inventore maiores.</p>
     </div>
   )
 }
