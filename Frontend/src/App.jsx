77354a49897058b2fc62a0f5d08d4a0976a4431b
rkishanCodes <rkishan.codes@gmail.com>
2024-07-23 12:44:40 +0530

    home bracnh home comp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,8 @@ const App = () => {
       <p>This is a paragraph.</p>
       change 2
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur laudantium similique nesciunt unde rerum ea vel, deleniti, corrupti quas atque fugiat animi nobis qui repellendus cupiditate deserunt culpa inventore maiores.</p>
+
+      changes in main from kishan desk of main branch
     </div>
   )
 }
