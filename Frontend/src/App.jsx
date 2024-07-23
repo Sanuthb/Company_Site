5d035082e59b94e06f84279a3aad7bdcaf4c7edc
Sanuthb <sanuthbibin@gmail.com>
2024-07-23 13:13:24 +0530

    changes made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      street2site
-      <p>This is a paragraph.</p>
-      change 2
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur laudantium similique nesciunt unde rerum ea vel, deleniti, corrupti quas atque fugiat animi nobis qui repellendus cupiditate deserunt culpa inventore maiores.</p>
-      new change 
+      <p>kishan brnach</p>
     </div>
   )
 }
