a872524869f710cda32105f1f3ec0c6e3de3d43a
rkishanCodes <rkishan.codes@gmail.com>
2024-08-19 00:02:33 +0530

    Coding section animation in going on

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Navbar from '../components/Navbar/Navbar'
-import Hero from '../components/Hero/Hero';
+import React, { useEffect, useRef } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
 import Services from "../components/Services/Services";
-import Coding from '../components/Coding/Coding';
+import Coding from "../components/Coding/Coding";
 
 const Home = () => {
   return (
-      <main>
-          <Navbar />
-          <Hero />
-          <About />
-          <Services/>
-          <Coding/>
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Coding />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
