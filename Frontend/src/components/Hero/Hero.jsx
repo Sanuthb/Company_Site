86aad672ab47f23c34abced3934ec30d8926eea5
Sanuthb <sanuthbibin@gmail.com>
2024-08-20 12:35:45 +0530

    New files and routes added

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
-import './Hero.css'
-import Hero_icons from '../Hero_components/Hero_icons';
+import React from "react";
+import "./Hero.css";
+import Hero_icons from "../Hero_components/Hero_icons";
+import { MdAutoGraph } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa6";
+import { FaGears } from "react-icons/fa6";
+import { TbBulb } from "react-icons/tb";
+import { IoIosColorPalette } from "react-icons/io";
+import { IoMegaphoneSharp } from "react-icons/io5";
+import { MdDesignServices } from "react-icons/md";
+import { FaPuzzlePiece } from "react-icons/fa6";
+import {motion} from 'framer-motion'
+import { createLoopAnimationVariants } from "../../Framer_Animation/Animation";
+
 
 const Hero = () => {
   return (
@@ -10,51 +21,162 @@ const Hero = () => {
           Empower
           <div class="hero_black_box1">
             <div class="hero_black_box_icons">
-              <div class="hero_icon">
-                <Hero_icons rotate={"rotate(55deg)"} />
-              </div>
-              <div class="hero_icon">
-                <Hero_icons rotate={"rotate(-65deg)"} />
-              </div>
-              <div class="hero_icon">
-                <Hero_icons rotate={"rotate(95deg)"} />
-              </div>
+              <motion.div
+                class="hero_icon"
+                variants={createLoopAnimationVariants(
+                  [0, -5, 0, -5, 0],
+                  [0, 0, 0, 0],
+                  [0, 360, 0]
+                )}
+                initial="initial"
+                animate="animate"
+              >
+                <Hero_icons
+                  rotate={"rotate(50deg)"}
+                  icons={<FaInstagram />}
+                  color={"#DB005B"}
+                />
+              </motion.div>
+              <motion.div
+                class="hero_icon"
+                variants={createLoopAnimationVariants(
+                  [0, 0, 0, 0, 0],
+                  [10, 0, 10, 0, 10]
+                )}
+                initial="initial"
+                animate="animate"
+              >
+                <Hero_icons
+                  rotate={"rotate(-65deg)"}
+                  icons={<MdAutoGraph />}
+                  color="#88D66C"
+                />
+              </motion.div>
+              <motion.div
+                class="hero_icon"
+                variants={createLoopAnimationVariants(
+                  [0, 10, 0, 10, 0],
+                  [0, 0, 0, 0, 0]
+                )}
+                initial="initial"
+                animate="animate"
+              >
+                <Hero_icons
+                  rotate={"rotate(-120deg)"}
+                  icons={<FaGears />}
+                  color="#939185"
+                />
+              </motion.div>
             </div>
           </div>
         </h1>
         <h1>
-          <div class="hero_icon" >
+          <motion.div
+            class="hero_icon"
+            style={{ marginRight: "6rem" }}
+            variants={createLoopAnimationVariants(
+              [0, 10, 0, 10, 0],
+              [0, 0, 0, 0, 0]
+            )}
+            initial="initial"
+            animate="animate"
+          >
             <Hero_icons
               rotate={"rotate(-15deg)"}
+              icons={<TbBulb />}
+              color="#F4CE14"
             />
-          </div>
+          </motion.div>
           your online
-          <div class="hero_icon">
-            <Hero_icons rotate={"rotate(35deg)"} />
-          </div>
+          <motion.div
+            class="hero_icon"
+            style={{ marginLeft: "6rem" }}
+            variants={createLoopAnimationVariants(
+              [0, 10, 0, 10, 0],
+              [0, 0, 0, 0, 0]
+            )}
+            initial="initial"
+            animate="animate"
+          >
+            <Hero_icons
+              rotate={"rotate(-114deg)"}
+              icons={<IoIosColorPalette />}
+              color="#FF8343"
+            />
+          </motion.div>
         </h1>
         <h1>
           <div class="hero_black_box2">
             <div
               class="hero_black_box_icons"
-              style={{ top: "110%", left: "18%", transform: "rotate(45deg)" }}
+              style={{
+                top: "140%",
+                left: "-120%",
+                transform: "rotate(-45deg)",
+              }}
             >
-              <div class="hero_icon">
-                <Hero_icons rotate={"rotate(90deg)"} />
-              </div>
-              <div class="hero_icon">
-                <Hero_icons rotate={"rotate(-65deg)"} />
-              </div>
-              <div class="hero_icon">
-                <Hero_icons rotate={"rotate(55deg)"} />
-              </div>
+              <motion.div
+                class="hero_icon"
+                variants={createLoopAnimationVariants(
+                  [0, 0, 0, 0, 0],
+                  [10, 0, 10, 0, 10]
+                )}
+                initial="initial"
+                animate="animate"
+              >
+                <Hero_icons
+                  rotate={"rotate(90deg)"}
+                  icons={<IoMegaphoneSharp />}
+                  color="#4158A6"
+                />
+              </motion.div>
+              <motion.div
+                class="hero_icon"
+                variants={createLoopAnimationVariants(
+                  [0, 10, 0, 10, 0],
+                  [0, 0, 0, 0, 0]
+                )}
+                initial="initial"
+                animate="animate"
+              >
+                <Hero_icons
+                  rotate={"rotate(-65deg)"}
+                  icons={<MdDesignServices />}
+                  color="#EB3678"
+                />
+              </motion.div>
+              <motion.div
+                class="hero_icon"
+                variants={createLoopAnimationVariants(
+                  [0, -5, 0, -5, 0],
+                  [0, 0, 0, 0],
+                  [0, 360, 0]
+                )}
+                initial="initial"
+                animate="animate"
+              >
+                <Hero_icons
+                  rotate={"rotate(-120deg)"}
+                  icons={<FaPuzzlePiece />}
+                  color="#36BA98"
+                />
+              </motion.div>
             </div>
           </div>
           presence
         </h1>
       </div>
+      <div
+        className="hero_content"
+        style={{
+          backgroundColor: "var(--clr--saturation-orange)",
+          justifyContent: "flex-start",
+        }}
+      >
+        <div className="codeeditior"></div>
+      </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
--- Frontend/src/components/Hero_components/Hero_icons.jsx
+++ Frontend/src/components/Hero_components/Hero_icons.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Hero_comp.css";
 
-const Hero_icons = ({ rotate}) => {
+const Hero_icons = ({ rotate,icons,color}) => {
   return (
     <div
       className="hero_icons"
-      style={{ transform: rotate}}
-    ></div>
+      style={{
+        transform: rotate,
+        color: color,
+        boxShadow: `-2px -1px 10px ${color}`,
+      }}
+    >
+      {icons}
+    </div>
   );
 };
 
