25241a9f0c0a97e7024b0ff5457d54a06c842cf9
rkishanCodes <rkishan.codes@gmail.com>
2024-08-22 19:44:57 +0530

    changes done

---
+++ Frontend/src/components/Pricing/Pricing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RoadMarquee from "./RoadMarquee.jsx";
+import Aeroplane from "./Aeroplane.jsx";
+
+const Pricing = () => {
+  return (
+    <>
+      <Aeroplane />
+      <RoadMarquee />
+    </>
+  );
+};
+
+export default Pricing;
