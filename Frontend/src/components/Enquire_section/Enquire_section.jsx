8afac015f9f1e6b288bfd8c697a9a1fea5de9e4c
Sanuthb <sanuthbibin@gmail.com>
2024-08-23 19:35:41 +0530

    navbar  section done

---
+++ Frontend/src/components/Enquire_section/Enquire_section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Enquire_section.css";
+
+const Enquire_section = () => {
+  return (
+    <div className="Enquire_section">
+      <h1>Let's Start a New Project Together</h1>
+      <button className="Inquire_btn">Inquire Now</button>
+    </div>
+  );
+}
+
+export default Enquire_section
--- Frontend/src/components/UI_components/Uitoggle.jsx
+++ Frontend/src/components/UI_components/Uitoggle.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef } from "react";
 import "./UIdesign.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Uitoggle = () => {
-  return (
-    <div className='uitogglebtn'>
-      <div className="circle"></div>
-    </div>
-  )
-}
+  const circleref = useRef();
 
-export default Uitoggle
+  useGSAP(() => {
+    gsap.to(circleref.current, {
+      duration: 1.3,
+      delay:1,
+      x: "200%",
+      yoyo: true,
+      repeat: -1,
+      ease: "poweri.inOut"
+    })
+  })
+
+  return (
+    <div className="uitogglebtn">
+      <div ref={circleref} className="circle"></div>
+    </div>
+  );
+};
+
+export default Uitoggle;
