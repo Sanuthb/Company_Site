36e5f092e5482ca57cba46e275ca994974823f39
rkishanCodes <rkishan.codes@gmail.com>
2024-08-23 19:02:01 +0530

    hero section issue class changed to className

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
 
 
   return (
-    <div class="hero">
+    <div className="hero">
       <div className="hero_content">
         <div className="hero_heading">
           <h1>
