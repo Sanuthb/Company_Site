fa5ee83656fb2577014e56d68b2bed58dfae4e46
Sanuthb <sanuthbibin@gmail.com>
2024-08-26 12:27:40 +0530

    entire webpage

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-import React, { useEffect, useRef } from "react";
->>>>>>> kishan
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
 import Services from "../components/Services/Services";
-<<<<<<< HEAD
 import Enquire_section from "../components/Enquire_section/Enquire_section";
-=======
 import Coding from "../components/Coding/Coding";
 import Pricing from "../components/Pricing/Pricing";
 import Testimonial from "../components/Testimonial/Testimonial";
->>>>>>> kishan
 
 const Home = () => {
   return (
@@ -22,13 +15,10 @@ const Home = () => {
       <Hero />
       <About />
       <Services />
-<<<<<<< HEAD
       <Enquire_section />
-=======
       <Coding />
       <Pricing />
       <Testimonial />
->>>>>>> kishan
     </main>
   );
 };
