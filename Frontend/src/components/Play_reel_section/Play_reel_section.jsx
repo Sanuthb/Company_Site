7645db9370907e946fb843576261805f1ed254d9
Sanuthb <165442353+Sanuthb@users.noreply.github.com>
2024-09-13 18:34:58 +0530

    c

--- Frontend/src/components/Play_reel_section/Play_reel_section.jsx
+++ Frontend/src/components/Play_reel_section/Play_reel_section.jsx
@@ -8,29 +8,32 @@ const Play_reel_section = () => {
   const videocontinerRef = useRef(null);
   const videoRef = useRef(null);
 
+  
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(min-width: 768px)"); 
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
 
-    if (mediaQuery.matches) {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: sectionRef.current,
-          markers: false,
-          start: "top top",
-          end: "bottom top",
-          scrub: 2,
-          pin: true,
-        },
-      });
+    if(mediaQuery.matches){
+      var tl = gsap.timeline({
+        scrollTrigger:{
+          trigger:videocontinerRef.current,
+          scroller:"body",
+          markers:true,
+          start:"top 0%",
+          end:"top -100%",
+          scrub:2,
+          pin:true
+        }
+      })
 
-      tl.to(videoRef.current, {
-        scale: 3.5,
-        duration: 1,
-        borderRadius: "0px",
-      });
+      tl.to(videoRef.current,{
+        scale:3.5,
+        duration:1,
+        borderRadius:"0px"
+      })
     }
-  }, []);
 
+
+  });
   return (
     <div className="Play_reel_section" ref={sectionRef}>
       <div className="play_reel_video" ref={videocontinerRef}>
